e0d74890d326d664a6b505173596f111656a7e03
Agop Kalikian <hagopk82@outlook.com>
2026-02-06 17:21:10 +0100

    Fix React root mounting and clean up App structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,34 @@
+import { useState } from "react";
 import AppLogo from "../components/AppLogo";
 
 function App() {
+  const [selectedAction, setSelectedAction] = useState("");
+
+  function handleChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    setSelectedAction(e.target.value);
+  }
+
   return (
     <>
-      <body
-        className={`min-h-screen bg-linear-to-br from-violet-900 to-slate-950 text-white`}
-      >
-        <header className="flex justify-center">
-          <AppLogo />
-        </header>
-      </body>
+      <header className="flex justify-center mt-4 mb-4">
+        <AppLogo />
+      </header>
+      <main className="mt-4 mb-4">
+        <label className="flex justify-center">
+          select the transaction form:
+          <select
+            defaultValue=""
+            name="selectedAction"
+            className="border-2 ml-2 rounded-md"
+            value={selectedAction}
+            onChange={handleChange}
+          >
+            <option className=""></option>
+            <option className="text-green-400">income</option>
+            <option className="text-red-400">expense</option>
+          </select>
+        </label>
+      </main>
     </>
   );
 }
--- src/hooks/useEntries.ts
+++ src/hooks/useEntries.ts
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { loadEntries } from "../services/storage";
-import type { Entry } from "../types/budget";
 
-export function useEntries() : void{
+export function useEntries(): void {
     const [entries, addEntry] = useState(loadEntries())
 
     useEffect(() => {
