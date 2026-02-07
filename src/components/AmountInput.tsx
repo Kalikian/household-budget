6ba758b7603e4e6481c468c917ac464a5280981f
Agop Kalikian <hagopk82@outlook.com>
2026-02-07 20:21:05 +0100

    feat(form): add AmountInput with validation for non-negative values and 2 decimals

--- src/components/AmountInput.tsx
+++ src/components/AmountInput.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AmountInput() {
+  const [amount, setAmount] = useState<string | number>("");
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const inputValue = e.target.value;
+    const regex = /^\d+(\.\d{0,2})?$/;
+    if (inputValue === "") {
+      setAmount("");
+      return;
+    }
+    if (!regex.test(inputValue)) {
+      return;
+    }
+    const numValue = Number(inputValue);
+    setAmount(numValue);
+  }
+
   return (
     <div>
-      <label>
+      <label htmlFor="amount">
         Amount:
-        <input className="" name="inputAmount" id="Amount"></input>
+        <input
+          className=""
+          name="inputAmount"
+          id="amount"
+          onChange={handleChange}
+          value={amount}
+          placeholder="Enter amount"
+          type="number"
+          min={0}
+          step={0.01}
+          inputMode="decimal"
+        ></input>
       </label>
     </div>
   );
